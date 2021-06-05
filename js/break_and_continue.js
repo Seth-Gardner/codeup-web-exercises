c45b3e7c2af3553b5feb2ae18c55d95ca8e35302
Seth Gardner <seth.gardner.a@gmail.com>
2021-06-04 19:55:49 -0500

    finished break and continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -13,3 +13,19 @@ for(let i = 1; i < 50; i++){
         console.log("Here is odd number: " + i)
     }
 }
+let i = 1
+while(i <= 16){
+    console.log(2 ** i);
+    i++
+}
+
+var coneTotal = Math.floor(Math.random() * 50) + 50;
+
+do{
+    let conesBought = Math.floor(Math.random() * 5) + 1;
+    if(coneTotal >= conesBought){
+        coneTotal = coneTotal - conesBought;
+        console.log(conesBought + " cones sold");
+    } else{ console.log("Cannot sell you " + conesBought + " I only have " + coneTotal)}
+} while(coneTotal > 0);
+console.log("Yay! I sold them all")
