8e264aa05859aed2387444f5887d7167d7925987
Seth Gardner <seth.gardner.a@gmail.com>
2021-06-07 11:25:42 -0500

    finisdhe iterating

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,36 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+    var names = ['Seth', 'Freddie', 'Zoe', 'Ram']
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names.length)
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    for(let i = 0; i < names.length; i++){
+        console.log(names[i])
+    }
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    for(let i = 0; i < names.length; i++){
+        console.log(names[i])
+    }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+    names.forEach(function(name){
+        console.log(name)
+    })
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +49,17 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-
+    function first(array){
+        console.log(array[0])
+    }
+    function second(array){
+        console.log(array[1])
+    }
+    function last(array){
+        console.log(array[array.length - 1])
+    }
+    let sampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    first(sampleArray)
+    second(sampleArray)
+    last(sampleArray)
 })();
