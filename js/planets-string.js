ebe6a753e8e8507117645f4a66246c318e069944
Seth Gardner <seth.gardner.a@gmail.com>
2021-06-07 15:15:48 -0500

    finished split-join

--- js/planets-string.js
+++ js/planets-string.js
@@ -12,7 +12,8 @@
      */
 
     // console.log(planetsArray);
-
+    planetsArray = planetsString.split("|")
+    console.log(planetsArray)
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
@@ -23,5 +24,15 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
-
+    var brStr = planetsArray.join("<br>")
+    console.log(brStr)
+    function makeList(array){
+        for(let i = 0; i < array.length; i++){
+            array.splice(i, 1, "<li>" + array[i] + "</li>")
+        }
+        array.unshift("<ul>");
+        array.push("</ul>");
+        console.log(array.join(""))
+    }
+    makeList(planetsArray)
 })();
