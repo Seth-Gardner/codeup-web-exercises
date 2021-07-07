fb03c35c48f08c1c97a6980d0d1a1aa78d8efbea
Seth Gardner <seth.gardner.a@gmail.com>
2021-07-07 09:24:06 -0500

    finishedmorning exercise

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -38,3 +38,43 @@ function capitalizeFirstLetter(arr){
     return(newArr)
 }
 console.log(capitalizeFirstLetter(['freddie', 'hi', 'samuel']))
+
+
+// July 7th 2021
+// TODO: Create a method on the object down below named getFullName that when called returns the users full name using the "this" key term.
+// Example: personOne.getFullName() ---- returns ----> "silvia floopertan"
+var personOne = {
+    firstName: "silvia",
+    lastName: "floopertan",
+    ageInYears: 34,
+    heightInInches: 65,
+    getFullName: function() {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+console.log(personOne.getFullName())
+// TODO: Create a method on the object down below named getNicelyFormattedFullName that when called returns the
+//  users full name with both first letters in the names being capitalized look at example down below for more reference.
+// Example: personOne.getNicelyFormattedFullName() ---- returns ----> "Dan Valdarez"
+var personTwo = {
+    firstName: "dan",
+    lastName: "valdarez",
+    ageInYears: 61,
+    heightInInches: 73,
+    getNicelyFormattedFullName: function() {
+        return capitalizeFirstLetter([this.firstName]) + ' ' + capitalizeFirstLetter([this.lastName])
+    }
+}
+console.log(personTwo.getNicelyFormattedFullName())
+// TODO: Create a method on the object below that gives us the behavior that the example implies.
+// Example: personThree.intro() ---- returns ----> "Hello, My name is Arlene Martinez and I am 25 years old"
+var personThree = {
+    firstName: "arlene",
+    lastName: "martinez",
+    ageInYears: 25,
+    heightInInches: 62,
+    helloMessage: function() {
+        return "Hello, My name is " + capitalizeFirstLetter([this.firstName]) + ' ' + capitalizeFirstLetter([this.lastName]) + "and I am " + this.ageInYears + " years old."
+    }
+}
+console.log(personThree.helloMessage())
