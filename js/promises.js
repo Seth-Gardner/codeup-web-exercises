581b692774d276e2079e5bfa8a7a37ba95d6a223
Seth Gardner <seth.gardner.a@gmail.com>
2021-07-23 14:30:34 -0500

    finished promises excericise

---
+++ js/promises.js
@@ -0,0 +1,18 @@
+function getUsersFromGithubAsJSON(username) {
+    return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': github_api}})
+        .then(function (response) {
+            return response.json()
+        });
+}
+getUsersFromGithubAsJSON(`Seth-Gardner`)
+    .then(function(response){
+        console.log(response[0].created_at)
+})
+
+function makeRequest(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("it worked")
+        }, time);
+    });
+}
